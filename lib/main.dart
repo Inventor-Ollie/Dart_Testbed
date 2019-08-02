d84eb3eeb0c1c5b4331ec670ffc81214e20d493a
Ollie Spearing <TheOllieSpearing@gmail.com>
2019-08-02 19:50:41 +0100

    New Welcome class that extends Stateless Widget Class update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-
-    new Material(
-      
-      color: Colors.deepOrange,
-
-      child: new Center (
-        child: new Text(
-          "Username: \nPassword: \nAre you a driving instructor?", 
-          style: TextStyle(fontSize: 30.0,), 
-          textDirection: TextDirection.ltr,))
-    )
-  );
+void main() {
+  runApp(MaterialApp(
+    home: Welcome(),
+    title: "Dryverr",
+  ));
 }
+
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.deepOrange,
+      child: Center(
+        child: Text(
+          "Welcome!!",
+          style: TextStyle(fontSize: 30.0),
+          textDirection: TextDirection.ltr,
+        ),
+      )
+    );
+  }
+} 
